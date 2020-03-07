f4a8f9a81d092c00ab7dfb27e75a51ff4b502661
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-03-07 22:00:14 +0100

    fixing

--- src/components/misc/PrivateRoute .js
+++ src/components/misc/PrivateRoute .js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Redirect } from 'react-router'
 
 const PrivateRoute  = ({component: Component, currentUser, ...rest}) => (
+  console.info('currentUser ',currentUser) ||
   <Route {...rest} render={props =>
         (currentUser)  ? (<Component {...props} />) 
         : (<Redirect to='/' {...props} />)
