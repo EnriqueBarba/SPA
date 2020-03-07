af108dc794d8441d4f31e39b4839834b86e86ead
EnriqueBarba <enrique.barbadillo26@gmail.com>
2020-03-07 17:15:17 +0100

    test_heroku_34

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -2,9 +2,8 @@ import axios from 'axios'
 import {PRODUCTS_KEY, ORDERS_KEY, CAT_KEY, PRODUCT_NAME} from './constants'
 
 const http = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_URL,
     withCredentials: true
-    
 })
 
 http.interceptors.response.use(
